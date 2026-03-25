430acf9c5a3f5911799942d4647a0617f9d50b17
LluisArjuna <lluisarjunav@gmail.com>
2026-03-25 11:35:56 +0100

    feat(app-routes): we define the routes we will use in this app and add  router-outlet in app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './pages/product-list-component/product-list-component';
+import { ProductDetailComponent } from './pages/product-detail-component/product-detail-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ProductListComponent},
+    {path: 'products', component: ProductListComponent},
+    {path: 'products/:id', component: ProductDetailComponent},
+];
