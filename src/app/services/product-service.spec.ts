e0070a521024580a8cc0a8b215dfc2cb39943b4b
LluisArjuna <lluisarjunav@gmail.com>
2026-03-25 13:28:50 +0100

    feat(test): add service tests to see if it returns products, if it finds a specific product and what hapends when you put a non existing ID.

--- src/app/services/product-service.spec.ts
+++ src/app/services/product-service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductService } from './product-service';
+import { products } from '../data/products';
+import { Product } from '../interfaces/product.interface';
 
 describe('ProductService', () => {
   let service: ProductService;
@@ -13,4 +15,26 @@ describe('ProductService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Verifiquen que devuelve la lista completa de productos.', () => {
+    const result = service.getProducts();
+    expect(result).toEqual(products);
+    expect(result.length).toBe(products.length);
+  });
+
+  it('Validen la búsqueda de un producto por ID.', () => {
+    const testProduct: Product = products[0];
+    const result = service.getProductById(testProduct.id);
+
+    expect(result).toEqual(testProduct);
+  });
+
+  it('Prueben el comportamiento cuando se busca un ID inexistente.', () => {
+    const result = service.getProductById('11');
+
+    expect(result).toBeUndefined();
+  });
 });
+
+ 
+
