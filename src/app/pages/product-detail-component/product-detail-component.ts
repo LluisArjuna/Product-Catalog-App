b127d74913bc020e3289f0bdbf203512c080c721
LluisArjuna <lluisarjunav@gmail.com>
2026-03-25 13:07:39 +0100

    feat(product-detail): add functionality on product details components to get the id, select the product with that id and print the product's details

--- src/app/pages/product-detail-component/product-detail-component.ts
+++ src/app/pages/product-detail-component/product-detail-component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/product-service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail-component',
-  imports: [],
-  templateUrl: './product-detail-component.html',
-  styleUrl: './product-detail-component.css',
+  imports: [CurrencyPipe],
+  templateUrl: './product-detail-component.html'
 })
-export class ProductDetailComponent {}
+export class ProductDetailComponent {
+  private readonly productService = inject(ProductService);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
+
+  productId = toSignal(this.route.paramMap.pipe(map(params => params.get('id'))));
+
+  product = computed(() => {
+    const id = this.productId();
+    if (!id) return undefined;
+    const product = this.productService.getProductById(id);
+    return product;
+  });
+
+  goBack() {
+    this.router.navigate(['/products']);
+  }
+}
