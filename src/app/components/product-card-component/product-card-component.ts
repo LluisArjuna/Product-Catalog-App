9e7ee011f163aaf937cd2922495eacc56a46c27b
LluisArjuna <lluisarjunav@gmail.com>
2026-03-25 12:04:28 +0100

    feat(product-card): add product card component the template structure and get data through signal

--- src/app/components/product-card-component/product-card-component.ts
+++ src/app/components/product-card-component/product-card-component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Product } from '../../interfaces/product.interface';
+import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card-component',
-  imports: [],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './product-card-component.html',
   styleUrl: './product-card-component.css',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  product = input.required<Product>();
+}
